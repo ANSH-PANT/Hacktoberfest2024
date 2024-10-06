668e96479670308fafdffb52b342976467b6577d
ANSH-PANT <131530705+ANSH-PANT@users.noreply.github.com>
2024-10-06 15:33:53 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,9 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
-  
+    {
+    id: 428,
+    fullname: "Ansh Pant",
+    username: "https://github.com/ANSH-PANT",
+  },
 ];
